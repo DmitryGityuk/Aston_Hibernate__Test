1f9ccc936f0f700cd1479366de0523c8b8f7037f
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2023-06-06 01:32:57 +0500

    added projectService

---
+++ src/main/java/com/aston_hibernate/service/ProjectService.java
@@ -0,0 +1,18 @@
+package com.aston_hibernate.service;
+
+import com.aston_hibernate.entity.Project;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    void addProject(Project project);
+
+    void updateProject(Project project);
+
+    void deleteProject(int id);
+
+    Project getProjectById(int id);
+
+    List<Project> listProjects();
+}
