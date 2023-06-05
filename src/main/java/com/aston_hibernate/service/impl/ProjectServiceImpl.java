fb54ec5d7e7cff8581d47ecb01214b161dbe6227
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2023-06-06 01:33:42 +0500

    added projectServiceImpl

---
+++ src/main/java/com/aston_hibernate/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,48 @@
+package com.aston_hibernate.service.impl;
+
+import com.aston_hibernate.dao.ProjectDao;
+import com.aston_hibernate.entity.Project;
+import com.aston_hibernate.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+    @Autowired
+    private ProjectDao projectDao;
+
+    @Override
+    @Transactional
+    public void addProject(Project project) {
+        this.projectDao.addProject(project);
+    }
+
+    @Override
+    @Transactional
+    public void updateProject(Project project) {
+        this.projectDao.updateProject(project);
+    }
+
+    @Override
+    @Transactional
+    public void deleteProject(int id) {
+        this.projectDao.deleteProject(id);
+    }
+
+    @Override
+    @Transactional
+    public Project getProjectById(int id) {
+        return this.projectDao.getProjectById(id);
+    }
+
+    @Override
+    @Transactional
+    public List<Project> listProjects() {
+        return this.projectDao.listProjects();
+    }
+}
+
